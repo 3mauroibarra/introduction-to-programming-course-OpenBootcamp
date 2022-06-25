1e58c74acdee8158952d885d5cb6582a8200f659
3mauroibarra <3mauroibarra@gmail.com>
2022-06-25 10:18:45 -0300

    Updated

--- Ejercicios tema 3/Segunda parte/Main.java
+++ Ejercicios tema 3/Segunda parte/Main.java
@@ -2,11 +2,14 @@ public class Main {
   public static void main(String[] args) {
     Coche miCoche = new Coche ();
     miCoche.AgregarPuerta();
+    miCoche.AgregarPuerta();
     System.out.println(miCoche.puertas);
 }
 
-  public static int suma(int a, int b) {
-    return a + b;
+  public static int suma(int a, int b, int c) {
+    int resultado;
+    return a + b + c;
+    System.out.println(resultado);
 }
 }
 
